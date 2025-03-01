37715eb93f2543056ef59d7a4cb0365c33a1b4af
Amid-1 <amid.1spbtj@gmail.com.com>
2025-03-01 20:55:04 +0300

    Обновлён Main.java и добавлены новые файлы

--- src/jdbc/Main.java
+++ src/jdbc/Main.java
@@ -9,24 +9,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Запуск программы...");
 
-        // Создаем объект сервиса, который взаимодействует с DAO
         UserService userService = new UserServiceImpl();
 
-        // 1. Создание таблицы
         userService.createUsersTable();
 
-        // 2. Добавление 4 пользователей
         userService.saveUser("Иван", "Иванов", (byte) 30);
-        System.out.println(" User с именем Иван добавлен в базу данных");
+        System.out.println(" User с именем Иван Иванов добавлен в базу данных");
 
         userService.saveUser("Вероника", "Сидорова", (byte) 25);
-        System.out.println(" User с именем Вероника добавлен в базу данных");
+        System.out.println(" User с именем Вероника Сидорова добавлен в базу данных");
 
         userService.saveUser("Петр", "Николаев", (byte) 40);
-        System.out.println(" User с именем Петр добавлен в базу данных");
+        System.out.println(" User с именем Петр Николаев добавлен в базу данных");
 
         userService.saveUser("Алена", "Козлова", (byte) 35);
-        System.out.println(" User с именем Алена добавлен в базу данных");
+        System.out.println(" User с именем Алена Козлова добавлен в базу данных");
 
         System.out.println("\n Список всех пользователей:");
         List<User> users = userService.getAllUsers();
